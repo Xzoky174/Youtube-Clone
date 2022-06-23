740e4519a7c767e0d4b32c3bb923d8559ab2d858
Xzoky174 <xzoky123@gmail.com>
2022-06-23 16:33:45 +0400

    Change findById to findOne for better error handling

--- pages/api/video/[id].ts
+++ pages/api/video/[id].ts
@@ -1,7 +1,6 @@
 import connect from "../../../utils/mongoConnect";
 
 import { getNextConnectInstance } from "../../../utils/getNextConnectInstance";
-import mongoose from "mongoose";
 
 const apiRoute = getNextConnectInstance();
 
@@ -9,35 +8,38 @@ apiRoute.get(async (req, res) => {
   const { id } = req.query;
 
   if (id !== undefined && id !== null) {
-    const { User, Video } = await connect();
+    const { Video } = await connect();
 
-    try {
-      const video = await Video.findById(
-        new mongoose.Types.ObjectId(id as unknown as string)
-      );
+    const video = await Video.findOne({ _id: id });
 
+    if (video) {
       const author = await (
         await fetch(`http://localhost:3000/api/user/${video.author_id}`)
       ).json();
 
-      video
-        ? res.status(200).json({
-            success: true,
-            data: {
-              video: { ...video._doc },
-              author: { ...author.data.user },
-            },
-          })
-        : res
-            .status(400)
-            .json({ success: false, error: "Video Not Found", data: {} });
-    } catch (e) {
-      console.log(e);
+      if (author) {
+        res.status(200).json({
+          success: true,
+          data: {
+            video: { ...video._doc },
+            author: { ...author.data.user },
+          },
+        });
+      } else {
+        // Author of video has been deleted, so delete the video.
+        await Video.findOneAndDelete({ _id: id });
 
-      res
-        .status(404)
-        .json({ success: false, error: "Video Not Found", data: {} });
+        res
+          .status(400)
+          .json({ success: false, error: "Video Not Found", data: {} });
+      }
+
+      return;
     }
+
+    res
+      .status(400)
+      .json({ success: false, error: "Video Not Found", data: {} });
   } else {
     res.status(400).json({ success: false, data: {} });
   }
