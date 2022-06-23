b6450ce9a8e7c309559b065029d115b23430e593
Xzoky174 <xzoky123@gmail.com>
2022-06-24 01:55:39 +0400

    Add like feature to client

--- pages/video/[id].tsx
+++ pages/video/[id].tsx
@@ -8,18 +8,29 @@ import Image from "next/image";
 import styles from "../../styles/modules/Video.module.css";
 import Link from "next/link";
 import Loader from "../components/Loader";
+import useSession from "../hooks/useSession";
 
 function Video() {
   const router = useRouter();
   const { id } = router.query;
 
+  const { user, loading } = useSession();
+
   const { data, error } = useSWR(`/api/video/${id}`, fetcher);
+
   const [videoDimensions, setVideoDimensions] = useState({
     width: 0,
     height: 0,
   });
+  const [liked, setLiked] = useState(false);
 
   useEffect(() => {
+    if (data) {
+      if (liked && data.data.video.users_liked.includes(user.id)) {
+        setLiked(false);
+      }
+    }
+
     setVideoDimensions({
       width: window.innerWidth - 20,
       height: 480,
@@ -30,7 +41,31 @@ function Video() {
         width: window.innerWidth - 20,
         height: 480,
       });
-  }, [data]);
+  }, [data, liked, user.id]);
+
+  const handleLike = async () => {
+    if (!loading) {
+      if (!user) {
+        router.push("/api/auth/signin");
+      } else if (data && !data.data.video.users_liked.includes(user.id)) {
+        setLiked(true);
+
+        await fetch("/api/video/add-like", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            video_id: data.data.video._id,
+          }),
+        });
+
+        setTimeout(() => {
+          setLiked(false);
+        }, 15000);
+      }
+    }
+  };
 
   return (
     <div>
@@ -58,6 +93,24 @@ function Video() {
             </div>
             <div className={styles.videoInfo}>
               <h1 className={styles.videoTitle}>{data.data.video.title}</h1>
+
+              <div className={styles.like}>
+                <div className={styles.likeSub} onClick={handleLike}>
+                  <Image
+                    className={styles.likeImage}
+                    src={"/like-icon.svg"}
+                    alt="like"
+                    width={32}
+                    height={32}
+                  />
+                  <p className={styles.likes}>
+                    {data.data.video.likes +
+                      (liked && !data.data.video.users_liked.includes(user.id)
+                        ? 1
+                        : 0)}
+                  </p>
+                </div>
+              </div>
 
               <Link href={`/user/${data.data.author.id}`} passHref>
                 <a className={styles.authorInfo}>
