c7d027cee32ad20ea77ee811fb19978f7c2bde32
Xzoky174 <xzoky123@gmail.com>
2022-06-25 02:24:02 +0400

    Fix unnessecary mongo connections

--- utils/mongoConnect.ts
+++ utils/mongoConnect.ts
@@ -3,18 +3,30 @@ import UserSchema from "./schemas/UserSchema";
 import VideoSchema from "./schemas/VideoSchema";
 
 export default async function connect() {
-  const videosConn = await mongoose
-    .createConnection(process.env.VIDEOS_MONGO_URI)
-    .asPromise();
-  const usersConn = await mongoose
-    .createConnection(process.env.USERS_MONGO_URI)
-    .asPromise();
+  const videosConn = (await mongoose.connect(process.env.VIDEOS_MONGO_URI))
+    .connection;
+  const usersConn = videosConn.useDb("Users");
 
-  const Video = videosConn.model("Video", VideoSchema);
-  const User = usersConn.model("User", UserSchema);
+  const Video =
+    videosConn.models.Video || videosConn.model("Video", VideoSchema);
+  const User = usersConn.models.User || usersConn.model("User", UserSchema);
 
   return {
     Video,
     User,
   };
+
+  // const videosConn = new mongoose.Mongoose();
+  // const usersConn = new mongoose.Mongoose();
+
+  // await videosConn.connect(process.env.VIDEOS_MONGO_URI);
+  // await usersConn.connect(process.env.USERS_MONGO_URI);
+
+  // const Video = videosConn.model("Video", VideoSchema);
+  // const User = usersConn.model("User", UserSchema);
+
+  // return {
+  //   Video,
+  //   User,
+  // };
 }
